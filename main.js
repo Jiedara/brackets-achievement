a697ffca66e71ba092677928fd31c9384137a9fd
Martin Villanove <m.villanove@laposte.net>
2016-06-27 13:55:59 +0200

    add key handling

--- main.js
+++ main.js
@@ -28,8 +28,19 @@ define(function (require, exports, module) {
 		// This holds the list of all default commands
 		Commands = brackets.getModule("command/Commands"),
 		// This lets us do things through the native app shell
-		NativeApp = brackets.getModule("utils/NativeApp");
+		NativeApp = brackets.getModule("utils/NativeApp"),
 
+		//other dependencies
+		AppInit = brackets.getModule("utils/AppInit"),
+        EditorManager = brackets.getModule("editor/EditorManager"),
+        KeyEvent = brackets.getModule("utils/KeyEvent");
+
+
+	var keyEventHandler = function ($event, editor, event) {
+		if ((event.type === "keydown") && (event.keyCode === KeyEvent.DOM_VK_TAB)) {
+            console.log("Tab pressed!");
+		}
+	};
 	/*
 	    The function which will be called when the command is
 	    executed (ie when users select the menu)
@@ -37,17 +48,36 @@ define(function (require, exports, module) {
 	function resetAchievement() {
 		// This is how you open a webpage in the current browser window
 		console.log('reset achievement');
+	}
 
+	function showAchievement() {
+		// This is how you open a webpage in the current browser window
+		console.log('show achievement');
+
+	}
+	function achieveSomething() {
+		// This is how you open a webpage in the current browser window
+		console.log('You just achieved something huuuge !');
 	}
 
 	CommandManager.register('Reset Achievement', 'jiedara.achievement.resetAchievement', resetAchievement);
+	CommandManager.register('Show Achievement', 'jiedara.achievement.showAchievement', showAchievement);
+	CommandManager.register('Achieve Something', 'jiedara.achievement.achieveSomething', achieveSomething);
 
 
 	//create my own menu
 	var menu = Menus.addMenu('Achievement', 'jiedara.achievement');
 
 	//submenu
+	menu.addMenuItem('jiedara.achievement.showAchievement', [], Menus.AFTER);
+	menu.addMenuItem('jiedara.achievement.achieveSomething', [], Menus.AFTER);
+
 	menu.addMenuDivider();
 	menu.addMenuItem('jiedara.achievement.resetAchievement', [], Menus.AFTER);
 
+
+    AppInit.appReady(function () {
+        var currentEditor = EditorManager.getCurrentFullEditor();
+        $(currentEditor).on('keyEvent', keyEventHandler);
+    });
 });
